968dca7c26c216474c19baaea33d8716a1e3e0d8
thingscouldbeworse <megal33tdude@gmail.com>
2017-02-26 12:08:51 -0500

    changed sql_connector.php to not store passwords in repo'd files

--- sql_connector.php
+++ sql_connector.php
@@ -1,6 +1,11 @@
 <?php
+
+$config = parse_ini_file( 'config.ini' );
+
+$password = $config['password'];
+
 DEFINE ('DB_USER','root');
-DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_PASSWORD', $password);
 DEFINE ('DB_HOST','localhost');
 DEFINE ('DB_NAME', 'cs499');
 
