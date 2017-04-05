50173061ec1ce7be263ec545eddce28ba6ebd422
Kirk Hardy <megal33tdude@gmail.com>
2017-04-05 15:43:04 -0400

    changed feature_connector to dynamically get docroot

--- feature_connector.php
+++ feature_connector.php
@@ -3,8 +3,11 @@
 * This file makes it easier to develop on multiple machines.
 * @author Daniel Dilger
 */
+$config = parse_ini_file( 'config.ini' );
 
-$features_loader_path =  $_SERVER["DOCUMENT_ROOT"].'/'.'sqsg6/Training_site/features/features_loader.php';
+$docroot = $config['docroot'];
+
+$features_loader_path =  $docroot .'/sqsg6/Training_site/features/features_loader.php';
 require_once($features_loader_path);
 
 ?>
