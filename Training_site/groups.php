3855ce28962278041ba1b22a4154fa52bec65cfb
Daniel <ddilger93@gmail.com>
2017-04-05 08:21:56 -0400

    Phone subscription service is now only available once you log in.

--- Training_site/groups.php
+++ Training_site/groups.php
@@ -1,7 +1,7 @@
 <?php include 'config/header.php';
 require_once('../sql_connector.php');
-$features_loader_path =  $_SERVER["DOCUMENT_ROOT"].'/'.'Training_site/features/features_loader.php';
-require_once($features_loader_path);
+//$features_loader_path =  $_SERVER["DOCUMENT_ROOT"].'/'.'Training_site/features/features_loader.php';
+//require_once($features_loader_path);
 ?>
 
 <?php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user'])){	//redirects to index page if user isn't a user
 ?>
 
 <?php
-feature_loader("test", $_SESSION["user"]);
+//feature_loader("test", $_SESSION["user"]);
 ?>
 
 
---
+++ Training_site/site_description_paragraph.php
@@ -0,0 +1,8 @@
+		<?php 
+		
+		echo '<div class="form-group" style="padding-top: 5px; padding-bottom: 5px">	';
+		echo '<form>';
+		echo '	<p> Log in to subscribe to our phone service. </p>';
+		echo '</form>';
+		echo '	</div>';
+?>
