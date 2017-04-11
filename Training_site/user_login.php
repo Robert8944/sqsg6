a3e07c42cafbb976deeaa33eebf439801a6033fd
Kirk Hardy <megal33tdude@gmail.com>
2017-04-11 00:51:49 -0400

    succsesfully merged changes between my old user_login and ddilger's

--- Training_site/user_login.php
+++ Training_site/user_login.php
@@ -5,9 +5,11 @@ require_once('../sql_connector.php');?>
 
 
 <?php
+/*
 if (isset($_SESSION['user'])){      //redirects if user isn't a user
     header('location:index.php');
 }
+*/
 if(isset($_POST['submit'])) {          //waits for button press
     $EmailError = False;
     $passwordError = False;
@@ -29,13 +31,15 @@ if(isset($_POST['submit'])) {          //waits for button press
 
     //updates info
     if ($passwordError == False and $EmailError == False) {
-        $stmt = $mysqli->prepare('SELECT UID,Name,is_admin FROM user WHERE email = ? AND password = ? LIMIT 1');
+        $stmt = $mysqli->prepare('SELECT UID,Name,level FROM user WHERE email = ? AND password = ? LIMIT 1');
         $stmt->bind_param("ss", $email,$password );
-        $stmt->bind_result($UID, $name, $admin);
+        $stmt->bind_result($UID, $name, $level);
         $stmt->execute();
         $results = $stmt->fetch();
         if ($results == 1) {
             session_start();
+            $admin = ($level == 5);
+            //echo "admin: ".$admin;
             if ($admin)
                 $_SESSION['priv'] = '1';
             else
@@ -59,43 +63,27 @@ if(isset($_POST['submit'])) {          //waits for button press
 <div class="container">
     <form  class= "form-horizontal"action="" method="post">
 
-
-            <div class="form-group" id="inputbox">
-                <label class="control-label col-sm-5">Email</label>
-                <div class="col-sm-7">
-                    <input type="email" name="email" size="30" </label>
-                </div>
+        <div class="form-group" id="inputbox">>
+            <label class="control-label col-sm-5">Email</label>
+            <div class="col-sm-7">
+                <input type="email" name="email" size="30" </label>
             </div>
+        </div>
 
-            <div class="form-group" id="inputbox">
-                <label class="control-label col-sm-5">Password</label>
-                <div class="col-sm-7">
+        <div class="form-group" id="inputbox">
+            <label class="control-label col-sm-5">Password</label>
+            <div class="col-sm-7">
                 <input type="password" name="password" size="30" /></label>
-                    </div>
             </div>
+        </div>
 
-            <div class="form-group" id="inputbox">
-                <div class="control-label col-sm-6">
-                <input class="btn btn-default" type="submit" name="submit" value="Send"/></label>
-                </div>
+        <div class="form-group" id="inputbox">
+            <div class="control-label col-sm-6">
+            <input class="btn btn-default" type="submit" name="submit" value="Sign in"/></label>
             </div>
-
-
+        </div>
 
     </form>
 </div>
 
-
-
-
-
-<?php
-/**
- * Created by PhpStorm.
- * User: Kevin Joiner
- * Date: 4/3/2016
- * Time: 3:05 AM
- */
-
-?>
 </html>
