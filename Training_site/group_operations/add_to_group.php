02098cd271df922b71d72793f46ef0fe50fe6f77
Daniel <ddilger93@gmail.com>
2017-04-27 15:55:10 -0400

    Comments added to the files in the group_operations folder.

--- Training_site/group_operations/add_to_group.php
+++ Training_site/group_operations/add_to_group.php
@@ -1,6 +1,10 @@
 <?php
 require_once('../../sql_connector.php');
+/*
+This file processes administrator and superuser's requests to add users to groups. Rather than performing a single command with the use of joined tables, it adds users by performing three seperate SQL commands.
 
+NOTE: if the user or the group entered does not actually exist, no error message is given. Nothing happens to the database when there is such a SQL error.
+ */
 
 $username = $_POST["username"];
 $group_name = $_POST["group_name"];
@@ -8,8 +12,11 @@ $group_name = $_POST["group_name"];
 $group_id = -1;
 $uid = -1;
 
+/*First fetch the primary key of the user who is to be added to the group.
+The primary key needs to be fetched because the superuser or admin
+identifies the user to add to a group by that user's email.
+*/
 $sql1 = "SELECT UID FROM user WHERE Email=\"".$username."\";";
-//echo "sql1: ".$sql1."<br />";
 $result1 = $mysqli->query($sql1);
 if($result1->num_rows > 0)
 {
@@ -19,8 +26,8 @@ if($result1->num_rows > 0)
 	}
 }
 
+//Select the primary key of the group whose name the superuser or admin enters.
 $sql2 = "SELECT id FROM groups where name=\"".$group_name."\";";
-//echo "sql2: ".$sql2."<br />";
 $result2 = $mysqli->query($sql2);
 if($result2->num_rows > 0)
 {
@@ -29,13 +36,10 @@ if($result2->num_rows > 0)
 		$group_id = $row["id"];
 	}
 }
+
+//Finally, add a new (group_id, uid) pair to the list of group members.
 $sql3 = "INSERT INTO group_members (group_id, uid) VALUES(".$group_id.", ".$uid.");";
 $result3 = $mysqli->query($sql3);
 
-//echo "group_name: ".$group_name."<br />";
-//echo "username: ".$username."<br />";
-//echo "group_id: ".$group_id."<br />";
-//echo "uid: ".$uid."<br />";
 echo "<meta http-equiv=\"refresh\" content=\"0; url=".$_SERVER['HTTP_REFERER']."\">";
-//echo "<meta http-equiv=\"refresh\" content=\"0; url=../admin_user_info.php\">";
 ?>
--- Training_site/group_operations/demote_from_leader.php
+++ Training_site/group_operations/demote_from_leader.php
@@ -1,15 +1,22 @@
 <?php
 require_once('../../sql_connector.php');
+/*
+This file processes an administrator's request to demote a group leader back to mere group member.
 
+NOTE: if the user or the group entered does not actually exist, no error message is given. Nothing happens to the database when there is such a SQL error.
+*/
 
 $username = $_POST["username"];
 $group_name = $_POST["group_name"];
 
 $group_id = -1;
 $uid = -1;
-
+/*
+First fetch the primary key of the user who is to be removed from the group.
+The primary key needs to be fetched because the admin identifies the 
+user to remove from the group by that user's email.
+*/
 $sql1 = "SELECT UID FROM user WHERE Email=\"".$username."\";";
-//echo "sql1: ".$sql1."<br />";
 $result1 = $mysqli->query($sql1);
 if($result1->num_rows > 0)
 {
@@ -19,8 +26,8 @@ if($result1->num_rows > 0)
 	}
 }
 
+//Next, select the primary key of the group whose name the admin enters.
 $sql2 = "SELECT id FROM groups where name=\"".$group_name."\";";
-//echo "sql2: ".$sql2."<br />";
 $result2 = $mysqli->query($sql2);
 if($result2->num_rows > 0)
 {
@@ -29,6 +36,8 @@ if($result2->num_rows > 0)
 		$group_id = $row["id"];
 	}
 }
+
+//Finally, demote the account from its leadership position.
 //Check if the account belongs to the group
 $sql3 = "SELECT * FROM group_members WHERE group_id=".$group_id." AND uid=".$uid.";";
 $result3 = $mysqli->query($sql3);
@@ -41,25 +50,14 @@ if($result3->num_rows > 0)
 if($belongsToGroup == False)
 {
 //If the user is not part of the group, no need to demote. Do nothing
-/*
-$sql4 = "INSERT INTO group_members (group_id, leader, uid) VALUES(".$group_id.",1, ".$uid.");";
-echo "sql4: ".$sql4."<br />";
-$result4 = $mysqli->query($sql4);
-*/
+
 }
 else
 {
-//Otherwise, simply promote the user
+//Otherwise, demote the user
 $sql4 = "UPDATE group_members SET leader=0 WHERE group_id=".$group_id." AND uid=".$uid.";";
-//echo "sql4: ".$sql4."<br />";
 $result4 = $mysqli->query($sql4);
 }
-//echo "group_name: ".$group_name."<br />";
-//echo "username: ".$username."<br />";
-//echo "group_id: ".$group_id."<br />";
-//echo "uid: ".$uid."<br />";
-//echo "Called";
 echo "<meta http-equiv=\"refresh\" content=\"0; url=../admin_user_info.php\">";
 
-//echo "<meta http-equiv=\"refresh\" content=\"0; url=../admin_user_info.php\">";
 ?>
